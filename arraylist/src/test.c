614f8d32853f32e5135211342f8f1e3062859c97
JeanRehr <jean.rehr@gmail.com>
2025-12-04 15:52:58 -0300

    add a test for simple structs

--- arraylist/src/test.c
+++ arraylist/src/test.c
@@ -19,12 +19,71 @@ struct test {
     float *b;
 };
 
-static struct test *test_alloc_ctor(int a, float b, char *objname);
-static void test_ctor(struct test *t, int a, float b, char *objname);
-static struct test test_ctor_by_val(int a, float b, char *objname);
-static void test_print(struct test *t);
-static void test_dtor(struct test *t);
-static void test_ptr_dtor(struct test **t);
+static struct test *test_alloc_ctor(int a, float b, char *objname) {
+    //printf("Constructor called! Creating object named %s\n", objname);
+    struct test *t = malloc(sizeof(struct test));
+    t->a = malloc(sizeof(a));
+    t->b = malloc(sizeof(b));
+    *t->a = a;
+    *t->b = b;
+    t->objname = objname;
+    return t;
+}
+
+static void test_ctor(struct test *t, int a, float b, char *objname) {
+    //printf("Constructor called! Creating object named %s\n", objname);
+    t->a = malloc(sizeof(a));
+    t->b = malloc(sizeof(b));
+    *t->a = a;
+    *t->b = b;
+    t->objname = objname;
+}
+
+static struct test test_ctor_by_val(int a, float b, char *objname) {
+    struct test t;
+    t.a = malloc(sizeof(a));
+    t.b = malloc(sizeof(b));
+    *t.a = a;
+    *t.b = b;
+    t.objname = objname;
+    return t;
+}
+
+static void test_print(struct test *t) {
+    //printf("t->a = %d\n", *t->a);
+    //printf("t->b = %f\n", *t->b);
+    printf("t->objname = %s\n", t->objname);
+}
+
+static void test_dtor(struct test *t) {
+    //printf("Destructor called for obj named %s!\n", t->objname);
+    if(!t) {
+        return;
+    }
+    
+    free(t->a);
+    free(t->b);
+}
+
+static void test_ptr_dtor(struct test **t) {
+    if(!t || !*t) {
+        return;
+    }
+
+    test_dtor(*t);
+    free(*t); 
+    *t = nullptr;
+}
+
+static void test_move_constructor(struct test *dst, struct test *src) {
+    dst->a = src->a;
+    dst->b = src->b;
+    dst->objname = src->objname;
+
+    src->a = nullptr;
+    src->b = nullptr;
+    src->objname = nullptr;
+}
 
 ARRAYLIST(struct test, test)
 
@@ -137,29 +196,32 @@ static void test_arraylist_push_back(void) {
     printf("Testing arraylist push_back function.\n");
     struct arraylist_test arrlisttest = arraylist_test_init(nullptr, test_dtor);
 
+
     struct test add1;
-    struct test add2;
-    struct test add3;
-    struct test add4;
-    struct test add5;
-
     test_ctor(&add1, 10, 0.5, "add1");
-    test_ctor(&add2, 11, 0.6, "add2");
-    test_ctor(&add3, 12, 0.7, "add3");
-    test_ctor(&add4, 13, 0.8, "add4");
-    test_ctor(&add5, 14, 0.9, "add5");
-
     arraylist_test_push_back(&arrlisttest, add1);
     assert(arrlisttest.size == 1);
+
+    struct test add2;
+    test_ctor(&add2, 11, 0.6, "add2");
     arraylist_test_push_back(&arrlisttest, add2);
     assert(arrlisttest.size == 2);
     assert(arrlisttest.capacity == 2);
+
+    struct test add3;
+    test_ctor(&add3, 12, 0.7, "add3");
     arraylist_test_push_back(&arrlisttest, add3);
     assert(arrlisttest.size == 3);
     assert(arrlisttest.capacity == 4);
+
+    struct test add4;
+    test_ctor(&add4, 13, 0.8, "add4");
     arraylist_test_push_back(&arrlisttest, add4);
     assert(arrlisttest.size == 4);
     assert(arrlisttest.capacity == 4);
+
+    struct test add5;
+    test_ctor(&add5, 14, 0.9, "add5");
     arraylist_test_push_back(&arrlisttest, add5);
     assert(arrlisttest.size == 5);
     assert(arrlisttest.capacity == 8);
@@ -168,39 +230,43 @@ static void test_arraylist_push_back(void) {
     assert(arrlisttest.capacity == 11);
 
     struct test add6;
-    struct test add7;
-    struct test add8;
-    struct test add9;
-    struct test add10;
-    struct test add11;
-    struct test add12;
-
     test_ctor(&add6, 15, 1.0, "add6");
-    test_ctor(&add7, 15, 1.0, "add7");
-    test_ctor(&add8, 15, 1.0, "add8");
-    test_ctor(&add9, 15, 1.0, "add9");
-    test_ctor(&add10, 15, 1.0, "add10");
-    test_ctor(&add11, 15, 1.0, "add11");
-    test_ctor(&add12, 15, 1.0, "add12");
-
     arraylist_test_push_back(&arrlisttest, add6);
     assert(arrlisttest.size == 6);
     assert(arrlisttest.capacity == 11);
+
+    struct test add7;
+    test_ctor(&add7, 15, 1.0, "add7");
     arraylist_test_push_back(&arrlisttest, add7);
     assert(arrlisttest.size == 7);
     assert(arrlisttest.capacity == 11);
+
+    struct test add8;
+    test_ctor(&add8, 15, 1.0, "add8");
     arraylist_test_push_back(&arrlisttest, add8);
     assert(arrlisttest.size == 8);
     assert(arrlisttest.capacity == 11);
+
+    struct test add9;
+    test_ctor(&add9, 15, 1.0, "add9");
     arraylist_test_push_back(&arrlisttest, add9);
     assert(arrlisttest.size == 9);
     assert(arrlisttest.capacity == 11);
+
+    struct test add10;
+    test_ctor(&add10, 15, 1.0, "add10");
     arraylist_test_push_back(&arrlisttest, add10);
     assert(arrlisttest.size == 10);
     assert(arrlisttest.capacity == 11);
+
+    struct test add11;
+    test_ctor(&add11, 15, 1.0, "add11");
     arraylist_test_push_back(&arrlisttest, add11);
     assert(arrlisttest.size == 11);
     assert(arrlisttest.capacity == 11);
+
+    struct test add12;
+    test_ctor(&add12, 15, 1.0, "add12");
     arraylist_test_push_back(&arrlisttest, add12);
     assert(arrlisttest.size == 12);
     assert(arrlisttest.capacity == (11 * 2));
@@ -877,6 +943,316 @@ static void test_arraylist_ptr_emplace_back_slot(void) {
     printf("arraylist_ptr emplace_back_slot passed all tests.\n");
 }
 
+struct test_simple {
+    char *objname;
+    int a;
+    float b;
+};
+
+static void test_simple_ctor(struct test_simple *t, int a, float b, char *objname) {
+    //printf("Constructor called! Creating object named %s\n", objname);
+    t->a = a;
+    t->b = b;
+    t->objname = objname;
+}
+
+static struct test_simple test_simple_ctor_by_val(int a, float b, char *objname) {
+    struct test_simple t;
+    t.a = a;
+    t.b = b;
+    t.objname = objname;
+    return t;
+}
+
+static void test_simple_ptr_dtor(struct test_simple **t) {
+    if(!t || !*t) {
+        return;
+    }
+
+    free(*t); 
+    *t = nullptr;
+}
+
+ARRAYLIST(struct test_simple, test_simple)
+
+static void test_simple_arraylist_push_back(void) {
+    printf("Testing arraylist push_back function.\n");
+    struct arraylist_test_simple arrlisttest = arraylist_test_simple_init(nullptr, nullptr);
+
+
+    struct test_simple add1;
+    test_simple_ctor(&add1, 10, 0.5, "add1");
+    arraylist_test_simple_push_back(&arrlisttest, add1);
+    assert(arrlisttest.size == 1);
+
+    struct test_simple add2;
+    test_simple_ctor(&add2, 11, 0.6, "add2");
+    arraylist_test_simple_push_back(&arrlisttest, add2);
+    assert(arrlisttest.size == 2);
+    assert(arrlisttest.capacity == 2);
+
+    struct test_simple add3;
+    test_simple_ctor(&add3, 12, 0.7, "add3");
+    arraylist_test_simple_push_back(&arrlisttest, add3);
+    assert(arrlisttest.size == 3);
+    assert(arrlisttest.capacity == 4);
+
+    struct test_simple add4;
+    test_simple_ctor(&add4, 13, 0.8, "add4");
+    arraylist_test_simple_push_back(&arrlisttest, add4);
+    assert(arrlisttest.size == 4);
+    assert(arrlisttest.capacity == 4);
+
+    struct test_simple add5;
+    test_simple_ctor(&add5, 14, 0.9, "add5");
+    arraylist_test_simple_push_back(&arrlisttest, add5);
+    assert(arrlisttest.size == 5);
+    assert(arrlisttest.capacity == 8);
+
+    arraylist_test_simple_reserve(&arrlisttest, 11);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add6;
+    test_simple_ctor(&add6, 15, 1.0, "add6");
+    arraylist_test_simple_push_back(&arrlisttest, add6);
+    assert(arrlisttest.size == 6);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add7;
+    test_simple_ctor(&add7, 15, 1.0, "add7");
+    arraylist_test_simple_push_back(&arrlisttest, add7);
+    assert(arrlisttest.size == 7);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add8;
+    test_simple_ctor(&add8, 15, 1.0, "add8");
+    arraylist_test_simple_push_back(&arrlisttest, add8);
+    assert(arrlisttest.size == 8);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add9;
+    test_simple_ctor(&add9, 15, 1.0, "add9");
+    arraylist_test_simple_push_back(&arrlisttest, add9);
+    assert(arrlisttest.size == 9);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add10;
+    test_simple_ctor(&add10, 15, 1.0, "add10");
+    arraylist_test_simple_push_back(&arrlisttest, add10);
+    assert(arrlisttest.size == 10);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add11;
+    test_simple_ctor(&add11, 15, 1.0, "add11");
+    arraylist_test_simple_push_back(&arrlisttest, add11);
+    assert(arrlisttest.size == 11);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple add12;
+    test_simple_ctor(&add12, 15, 1.0, "add12");
+    arraylist_test_simple_push_back(&arrlisttest, add12);
+    assert(arrlisttest.size == 12);
+    assert(arrlisttest.capacity == (11 * 2));
+
+    arraylist_test_simple_deinit(&arrlisttest);
+    printf("arraylist push_back passed all tests.\n");
+}
+
+static void test_simple_arraylist_emplace_back_slot(void) {
+    printf("Testing arraylist emplace_back_slot function.\n");
+    struct arraylist_test_simple arrlisttest = arraylist_test_simple_init(nullptr, nullptr);
+
+    // ways that emplace_back can be used with arraylist of values:
+
+    // can be added like this, most efficient as it is constructed inside the container:
+    struct test_simple *add1 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+
+    // emplace_back_slot may return nullptr, needs to be checked in real scenarios
+    if (!add1) {
+        assert(false && "emplace back returned null");
+    }
+
+    test_simple_ctor(add1, 10, 0.5, "add1");
+    assert(arrlisttest.size == 1);
+
+    struct test_simple *add1_same = arraylist_test_simple_at(&arrlisttest, 0);
+    assert(add1_same == add1);
+
+    // can be added like this, copy into slot:
+    struct test_simple add2;
+    test_simple_ctor(&add2, 11, 0.6, "add2");
+    *arraylist_test_simple_emplace_back_slot(&arrlisttest) = add2;
+    assert(arrlisttest.size == 2);
+    assert(arrlisttest.capacity == 2);
+
+    // could be added by assigning a struct literal, if the struct had simple fields:
+    //*arraylist_test_simple_emplace_back_slot(&arrlisttest) = (struct test_simple){ .a = ...};
+
+    // can be added with a constructor that returns a struct by value:
+    *arraylist_test_simple_emplace_back_slot(&arrlisttest) = test_simple_ctor_by_val(12, 0.7, "add3");
+    assert(arrlisttest.size == 3);
+    assert(arrlisttest.capacity == 4);
+
+    // can fill slots individually:
+    struct test_simple *add4 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    add4->a = 13;
+    add4->b = 0.8;
+    add4->objname = "add4";
+    assert(arrlisttest.size == 4);
+    assert(arrlisttest.capacity == 4);
+    struct test_simple *add4_same = arraylist_test_simple_at(&arrlisttest, 3);
+    assert(add4_same == add4);
+
+    struct test_simple *add5 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add5, 14, 0.9, "add5");
+    assert(arrlisttest.size == 5);
+    assert(arrlisttest.capacity == 8);
+    struct test_simple *add5_same = arraylist_test_simple_at(&arrlisttest, 4);
+    assert(add5_same == add5);
+
+    arraylist_test_simple_reserve(&arrlisttest, 11);
+    assert(arrlisttest.capacity == 11);
+
+    struct test_simple *add6 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add6, 14, 0.9, "add6");
+    assert(arrlisttest.size == 6);
+    assert(arrlisttest.capacity == 11);
+    struct test_simple *add6_same = arraylist_test_simple_at(&arrlisttest, 5);
+    assert(add6_same == add6);
+
+    struct test_simple *add7 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add7, 14, 0.9, "add7");
+    assert(arrlisttest.size == 7);
+    struct test_simple *add7_same = arraylist_test_simple_at(&arrlisttest, 6);
+    assert(add7_same == add7);
+
+    struct test_simple *add8 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add8, 14, 0.9, "add8");
+    assert(arrlisttest.size == 8);
+    struct test_simple *add8_same = arraylist_test_simple_at(&arrlisttest, 7);
+    assert(add8_same == add8);
+
+    struct test_simple *add9 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add9, 14, 0.9, "add9");
+    assert(arrlisttest.size == 9);
+    struct test_simple *add9_same = arraylist_test_simple_at(&arrlisttest, 8);
+    assert(add9_same == add9);
+
+    struct test_simple *add10 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add10, 14, 0.9, "add10");
+    assert(arrlisttest.size == 10);
+    struct test_simple *add10_same = arraylist_test_simple_at(&arrlisttest, 9);
+    assert(add10_same == add10);
+
+    struct test_simple *add11 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add11, 14, 0.9, "add11");
+    assert(arrlisttest.size == 11);
+    assert(arrlisttest.capacity == 11);
+    struct test_simple *add11_same = arraylist_test_simple_at(&arrlisttest, 10);
+    assert(add11_same == add11);
+
+    struct test_simple *add12 = arraylist_test_simple_emplace_back_slot(&arrlisttest);
+    test_simple_ctor(add12, 14, 0.9, "add11");
+    assert(arrlisttest.size == 12);
+    assert(arrlisttest.capacity == (11 * 2));
+    struct test_simple *add12_same = arraylist_test_simple_at(&arrlisttest, 11);
+    assert(add12_same == add12);
+
+    arraylist_test_simple_deinit(&arrlisttest);
+    printf("arraylist emplace_back_slot passed all tests.\n");
+}
+
+// An example of a container of pointers of struct test_simple
+ARRAYLIST(struct test_simple*, test_simple_ptr)
+
+static void test_simple_arraylist_ptr_push_back(void) {
+    printf("Testing arraylist_ptr push_back function.\n");
+    struct arraylist_test_simple_ptr arrlisttestptr = arraylist_test_simple_ptr_init(nullptr, test_simple_ptr_dtor);
+
+    struct test_simple *add1 = malloc(sizeof(struct test_simple));
+    test_simple_ctor(add1, 10, 0.5, "add1");
+
+    struct test_simple *add2 = malloc(sizeof(struct test_simple));
+    test_simple_ctor(add2, 11, 0.6, "add2");
+
+    struct test_simple *add3 = malloc(sizeof(struct test_simple));
+
+    *add3 = test_simple_ctor_by_val(12, 0.7, "add3");
+
+    struct test_simple *add4 = malloc(sizeof(struct test_simple));
+    test_simple_ctor(add4, 13, 0.8, "add4");
+
+    struct test_simple *add5 = malloc(sizeof(struct test_simple));
+    test_simple_ctor(add5, 14, 0.9, "add5");
+
+
+    arraylist_test_simple_ptr_push_back(&arrlisttestptr, add1);
+    assert(arrlisttestptr.size == 1);
+    arraylist_test_simple_ptr_push_back(&arrlisttestptr, add2);
+    assert(arrlisttestptr.size == 2);
+    assert(arrlisttestptr.capacity == 2);
+    arraylist_test_simple_ptr_push_back(&arrlisttestptr, add3);
+    assert(arrlisttestptr.size == 3);
+    assert(arrlisttestptr.capacity == 4);
+    arraylist_test_simple_ptr_push_back(&arrlisttestptr, add4);
+    assert(arrlisttestptr.size == 4);
+    assert(arrlisttestptr.capacity == 4);
+    arraylist_test_simple_ptr_push_back(&arrlisttestptr, add5);
+    assert(arrlisttestptr.size == 5);
+    assert(arrlisttestptr.capacity == 8);
+
+    arraylist_test_simple_ptr_reserve(&arrlisttestptr, 11);
+    assert(arrlisttestptr.capacity == 11);
+
+    arraylist_test_simple_ptr_deinit(&arrlisttestptr);
+    // free(add1); // double-free, arraylist owns the pointers to it and frees it
+
+    // add1 is still not null, but points to freed memory, the dtor passed to arraylist sets the
+    // freed pointer to null, as the arraylist shallow copies, so dtor will set only the copy
+    // inside it to null 
+    assert(add1);
+    assert(!arrlisttestptr.data);
+    printf("arraylist_ptr push_back passed all tests.\n");
+}
+
+static void test_simple_arraylist_ptr_emplace_back_slot(void) {
+    printf("Testing arraylist_ptr emplace_back_slot function.\n");
+    struct arraylist_test_simple_ptr arrlisttestptr = arraylist_test_simple_ptr_init(nullptr, test_simple_ptr_dtor);
+
+    // ways that emplace_back can be used with arraylist of pointers to values:
+
+    // can be added like this:
+    struct test_simple *add1 = malloc(sizeof(struct test_simple));
+    test_simple_ctor(add1, 10, 0.5, "add1");
+    
+    // get the pointer to a slot where a struct test_simple pointer can be stored
+    struct test_simple **slot_add1 = arraylist_test_simple_ptr_emplace_back_slot(&arrlisttestptr);
+
+    // store it
+    *slot_add1 = add1;
+
+    assert(arrlisttestptr.size == 1);
+
+    struct test_simple **add1_same = arraylist_test_simple_ptr_at(&arrlisttestptr, 0);
+    // at returns a struct test_simple **, a pointer where a struct test_simple * is stored
+    assert(*add1_same == add1);
+
+    // can be added like this as well:
+    struct test_simple *add2 = malloc(sizeof(struct test_simple));
+    test_simple_ctor(add2, 11, 0.6, "add2");
+    *arraylist_test_simple_ptr_emplace_back_slot(&arrlisttestptr) = add2;
+
+    struct test_simple **add2_same = arraylist_test_simple_ptr_at(&arrlisttestptr, 1);
+    assert(*add2_same == add2);
+
+    // one-liner with an init function that returns a malloced and constructed object
+    *arraylist_test_simple_ptr_emplace_back_slot(&arrlisttestptr) = malloc(sizeof(test_simple_ctor_by_val(12, 0.7, "add3")));
+    assert(arrlisttestptr.size == 3);
+
+    arraylist_test_simple_ptr_deinit(&arrlisttestptr);
+    printf("arraylist_ptr emplace_back_slot passed all tests.\n");
+}
+
 ARRAYLIST(long, long)
 
 static void test_arraylist_bufferoverflow() {
@@ -953,6 +1329,12 @@ int main(void) {
     test_arraylist_swap();
     test_arraylist_clear();
 
+    test_simple_arraylist_push_back();
+    test_simple_arraylist_emplace_back_slot();
+
+    test_simple_arraylist_ptr_push_back();
+    test_simple_arraylist_ptr_emplace_back_slot();
+
     //test_arraylist_insert_from_to();
 
     test_arraylist_ptr_push_back();
@@ -962,62 +1344,6 @@ int main(void) {
 
     test_arraylist_bufferoverflow();
     test_arraylist_allocating_zero();
-    
+
     return 0;
-}
-
-static struct test *test_alloc_ctor(int a, float b, char *objname) {
-    //printf("Constructor called! Creating object named %s\n", objname);
-    struct test *t = malloc(sizeof(struct test));
-    t->a = malloc(sizeof(a));
-    t->b = malloc(sizeof(b));
-    *t->a = a;
-    *t->b = b;
-    t->objname = objname;
-    return t;
-}
-
-static void test_ctor(struct test *t, int a, float b, char *objname) {
-    //printf("Constructor called! Creating object named %s\n", objname);
-    t->a = malloc(sizeof(a));
-    t->b = malloc(sizeof(b));
-    *t->a = a;
-    *t->b = b;
-    t->objname = objname;
-}
-
-static struct test test_ctor_by_val(int a, float b, char *objname) {
-    struct test t;
-    t.a = malloc(sizeof(a));
-    t.b = malloc(sizeof(b));
-    *t.a = a;
-    *t.b = b;
-    t.objname = objname;
-    return t;
-}
-
-static void test_print(struct test *t) {
-    //printf("t->a = %d\n", *t->a);
-    //printf("t->b = %f\n", *t->b);
-    printf("t->objname = %s\n", t->objname);
-}
-
-static void test_dtor(struct test *t) {
-    //printf("Destructor called for obj named %s!\n", t->objname);
-    if(!t) {
-        return;
-    }
-    
-    free(t->a);
-    free(t->b);
-}
-
-static void test_ptr_dtor(struct test **t) {
-    if(!t || !*t) {
-        return;
-    }
-
-    test_dtor(*t);
-    free(*t); 
-    *t = nullptr;
 }
