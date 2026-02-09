572ce6bb51be9437cb774ba2fe4f229198bf0b10
JeanRehr <jean.rehr@gmail.com>
2026-02-08 22:23:50 -0300

    usage/testing of avltree

---
+++ avltree/avlusage.c
@@ -0,0 +1,23 @@
+#include "allocator.h"
+#include "avltree.h"
+#include <stdio.h>
+
+AVLTREE_TYPE(int, ints_tree)
+AVLTREE_DECL(int, ints_tree)
+AVLTREE_IMPL(int, ints_tree, avltree_noop_deinit)
+
+int comparator_ints(const int *a, const int *b) {
+    if (a == b) return 0;
+    if (a < b) return -1;
+    return 1;
+}
+
+int main(void) {
+    struct Allocator alloc = allocator_get_default();
+    struct avltree_ints_tree tree = ints_tree_init(alloc, comparator_ints);
+
+    printf("%p\n", tree.alloc.malloc);
+
+    printf("%s\n", __FILE__);
+    return 0;
+}
