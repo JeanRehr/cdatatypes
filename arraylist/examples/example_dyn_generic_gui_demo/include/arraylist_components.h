55a9e33f110c45d4317bdc85fd5494986eab9d3e
JeanRehr <jeanrehr@gmail.com>
2026-03-18 22:40:34 -0300

    add an arraylist of component base pointers

---
+++ arraylist/examples/example_dyn_generic_gui_demo/include/arraylist_components.h
@@ -0,0 +1,24 @@
+#ifndef ARRAYLIST_COMPONENTS_H
+#define ARRAYLIST_COMPONENTS_H
+
+#include "component_base.h"
+
+/*
+ * We are defining here an arraylist of pointer base interfaces of components, the linkage has to be
+ * overrided to extern to allow it to be used throughout all codebase by just including this header
+ */
+
+#define ARRAYLIST_LINKAGE extern
+#include "arraylist.h"
+
+// As there is no way to know ahead of time which component we are looking at (textbox, button, etc)
+// The destructor has to be a function pointer
+ARRAYLIST_TYPE_DYN(struct component_base *, components)
+ARRAYLIST_DECL_DYN(struct component_base *, components)
+
+// Every macro declared and the type used will be expanded here
+
+// Function that essentially dispatches to the correct implementation given the component
+void screen_base_ptr_dtor(struct component_base **base_ptr, struct Allocator *alloc);
+
+#endif // ARRAYLIST_COMPONENTS_H
---
+++ arraylist/examples/example_dyn_generic_gui_demo/src/arraylist_components.c
@@ -0,0 +1,33 @@
+#include "arraylist_components.h"
+
+/*
+ * We have to undef ARRAYLIST_LINKAGE here so that there is no linkage, it has to be the normal linkage
+ */
+
+#undef ARRAYLIST_LINKAGE
+#define ARRAYLIST_LINKAGE
+#include "arraylist.h"
+
+ARRAYLIST_DECL_DYN(struct component_base *, components)
+
+// Every macro implementation of the type used will be expanded here
+
+// Destructor function that frees component_base pointers
+// This should be used in the init of arraylist of component_base pointers
+void component_base_ptr_dtor(struct component_base **base_ptr, struct Allocator *alloc) {
+    if (!base_ptr || !*base_ptr) {
+        return;
+    }
+
+    struct component_base *base = *base_ptr;
+
+    // Call the component's own destructor if it has one
+    if (base->component_deinit) {
+        base->component_deinit(base_ptr, alloc);
+    } else {
+        // if no destructor, attempt to free with stored size
+        // this maybe may lead to partial/incorrect frees
+        alloc->free(base, base->allocated_size, alloc->ctx);
+        *base_ptr = NULL;
+    }
+}
