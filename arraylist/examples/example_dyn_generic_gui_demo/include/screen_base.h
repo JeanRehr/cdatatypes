ff1433c0730b69d9d3cac1b02e631dc384573b94
JeanRehr <jeanrehr@gmail.com>
2026-03-18 22:20:16 -0300

    an interface screen base

--- arraylist/examples/example_dyn_generic_gui_demo/include/screen_base.h
+++ arraylist/examples/example_dyn_generic_gui_demo/include/screen_base.h
@@ -1,4 +1,14 @@
 #ifndef SCREEN_BASE_H
 #define SCREEN_BASE_H
 
+#include "allocator.h"
+
+// All common behavior that a screen could have
+struct screen_base {
+    void (*screen_render)(struct screen_base *self);
+    void (*screen_deinit)(struct screen_base **self_ptr, struct Allocator *alloc);
+    const char *screen_name;
+    size_t allocated_size;
+};
+
 #endif // SCREEN_BASE_H
