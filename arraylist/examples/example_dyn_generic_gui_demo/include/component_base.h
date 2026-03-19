4dc7db733282e48277b28c48d770262340aff59c
JeanRehr <jeanrehr@gmail.com>
2026-03-18 22:20:09 -0300

    an interface component base

--- arraylist/examples/example_dyn_generic_gui_demo/include/component_base.h
+++ arraylist/examples/example_dyn_generic_gui_demo/include/component_base.h
@@ -1,4 +1,14 @@
 #ifndef COMPONENT_BASE_H
 #define COMPONENT_BASE_H
 
+#include "allocator.h"
+
+// All common behavior that a component could have
+struct component_base {
+    void (*component_render)(struct component_base *self);
+    void (*component_deinit)(struct component_base **self_ptr, struct Allocator *alloc);
+    const char *component_name;
+    size_t allocated_size;
+};
+
 #endif // COMPONENT_BASE_H
